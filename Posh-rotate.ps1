a1997cdff047458ea7eed85af130bc30b92c2b49
Malinda <malinda.rathnayake@outlook.com>
2023-11-16 14:57:04 -0500

    Update Posh-rotate.ps1
    Optimize and comments added with chatgpt

--- Posh-rotate.ps1
+++ Posh-rotate.ps1
@@ -4,64 +4,79 @@ function Compress-And-Archive {
         [int]$DaysThreshold = 30
     )
 
-    $ThresholdDate = (Get-Date).AddDays(-$DaysThreshold)
-    $ArchiveFolder = Join-Path -Path $Source -ChildPath "_Archive"
-    $7ZipExe = "C:\Program Files\7-Zip\7z.exe"  # Adjust the path to your 7zip executable
-
-    # Create the _Archive folder if it doesn't exist
-    if (-not (Test-Path -Path $ArchiveFolder -PathType Container)) {
-        New-Item -Path $ArchiveFolder -ItemType Directory
-    }
-
-    # Get files older than the specified days
-    $MatchedFiles = Get-ChildItem -LiteralPath $Source -Recurse | Where-Object { $_.LastWriteTime -lt $ThresholdDate }
-
-    # Move matched files to the _Archive folder
-    foreach ($file in $MatchedFiles) {
-        $RelativePath = $file.FullName.Substring($Source.Length + 1)
-        $Destination = Join-Path -Path $ArchiveFolder -ChildPath $RelativePath
-
-        # Create the destination directory if it doesn't exist
-        $DestDirectory = [System.IO.Path]::GetDirectoryName($Destination)
-        if (-not (Test-Path -Path $DestDirectory -PathType Container)) {
-            New-Item -Path $DestDirectory -ItemType Directory -Force
+    try {
+        # Validate Source path
+        if (-not (Test-Path -Path $Source -PathType Container)) {
+            throw "Source path '$Source' does not exist or is not a directory."
         }
 
-        Move-Item -Path $file.FullName -Destination $Destination -Force
+        $ThresholdDate = (Get-Date).AddDays(-$DaysThreshold)
+        $ArchiveFolder = Join-Path -Path $Source -ChildPath "_Archive"
+        $7ZipExe = "C:\Program Files\7-Zip\7z.exe"  # Adjust the path to your 7zip executable
+
+        # Create the _Archive folder if it doesn't exist
+        if (-not (Test-Path -Path $ArchiveFolder -PathType Container)) {
+            New-Item -Path $ArchiveFolder -ItemType Directory
+        }
+
+        # Get files older than the specified days
+        $MatchedFiles = Get-ChildItem -LiteralPath $Source -Recurse | Where-Object { $_.LastWriteTime -lt $ThresholdDate }
+
+        if ($MatchedFiles -eq $null) {
+            Write-Host "No logs older than $DaysThreshold days found in $Source. Exiting."
+            return
+        }
+
+        # Move matched files to the _Archive folder
+        foreach ($file in $MatchedFiles) {
+            $RelativePath = $file.FullName.Substring($Source.Length + 1)
+            $Destination = Join-Path -Path $ArchiveFolder -ChildPath $RelativePath
+
+            # Create the destination directory if it doesn't exist
+            $DestDirectory = [System.IO.Path]::GetDirectoryName($Destination)
+            if (-not (Test-Path -Path $DestDirectory -PathType Container)) {
+                New-Item -Path $DestDirectory -ItemType Directory -Force
+            }
+
+            Move-Item -Path $file.FullName -Destination $Destination -Force
+        }
+
+        Write-Host "Files moved to $ArchiveFolder"
+
+        $MovedFolders = $MatchedFiles | ForEach-Object { $_.DirectoryName }
+        $FoldersToRemove = Get-ChildItem -LiteralPath $Source -Directory | Where-Object { $MovedFolders -contains $_.FullName }
+
+        foreach ($folder in $FoldersToRemove) {
+            Remove-Item -Path $folder.FullName -Recurse -Force
+        }
+
+        # Compress each nested folder in _Archive using 7zip with higher compression
+        $NestedFolders = Get-ChildItem -Path $ArchiveFolder -Directory
+        foreach ($folder in $NestedFolders) {
+            $FolderPath = $folder.FullName
+            $ZipFilePath = "$FolderPath.zip"
+
+            # Compress the folder using 7zip with higher compression level (-mx9)
+            Start-Process -FilePath $7ZipExe -ArgumentList "a -tzip -mx9 `"$ZipFilePath`" `"$FolderPath`"" -Wait
+
+            Write-Host "Folder $FolderPath has been compressed to $ZipFilePath with higher compression"
+
+            # Remove the folder after compression
+            Remove-Item -Path $FolderPath -Recurse -Force
+            Write-Host "Folder $FolderPath has been removed."
+        }
+
+        Write-Host "Files moved to $ArchiveFolder, nested folders compressed"
     }
-
-    Write-Host "Files moved to $ArchiveFolder"
-
-    $MovedFolders = $MatchedFiles | ForEach-Object { $_.DirectoryName }
-    $FoldersToRemove = Get-ChildItem -LiteralPath $Source -Directory | Where-Object { $MovedFolders -contains $_.FullName }
-
-    foreach ($folder in $FoldersToRemove) {
-        Remove-Item -Path $folder.FullName -Recurse -Force
+    catch {
+        Write-Host "Error: $_"
     }
-
-    # Compress each nested folder in _Archive using 7zip with higher compression
-    $NestedFolders = Get-ChildItem -Path $ArchiveFolder -Directory
-    foreach ($folder in $NestedFolders) {
-        $FolderPath = $folder.FullName
-        $ZipFilePath = "$FolderPath.zip"
-
-        # Compress the folder using 7zip with higher compression level (-mx9)
-        Start-Process -FilePath $7ZipExe -ArgumentList "a -tzip `"$ZipFilePath`" `"$FolderPath`"" -Wait
-
-        Write-Host "Folder $FolderPath has been compressed to $ZipFilePath with higher compression"
-
-        # Remove the folder after compression
-        Remove-Item -Path $FolderPath -Recurse -Force
-        Write-Host "Folder $FolderPath has been removed."
-    }
-
-    Write-Host "Files moved to $ArchiveFolder, nested folders compressed"
 }
 
 # Define Paths to process
 $PathsToProcess = @(
     'D:\deviceLogs\',
-    'D:\ServerLogs\',
+    'D:\deviceLogs - Copy'
     # Add more paths as needed
 )
 
